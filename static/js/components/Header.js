06b65f848aa63690cdf3390997a482c52b61eb2e
kamy23711-png <kamy23711@gmail.com>
2026-07-21 21:54:41 +0900

    Update Header.js

--- static/js/components/Header.js
+++ static/js/components/Header.js
@@ -1,1 +1,20 @@
+import { Hamburger } from "./Hamburger.js";
 
+export function Header(){
+
+    const header = document.createElement("header");
+
+    header.className = "header";
+
+    header.innerHTML = `
+        <h1>Smart Factory Window</h1>
+    `;
+
+    header.append(
+        Hamburger(() => {
+            alert("メニューを開く")
+        })
+    )
+    return header;
+
+}
