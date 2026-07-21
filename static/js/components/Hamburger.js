5499ec03915f5a6ebdf461f60197d73ab74cf412
kamy23711-png <kamy23711@gmail.com>
2026-07-21 21:55:31 +0900

    Implement Hamburger menu component
    Add a Hamburger component for toggle functionality.

--- static/js/components/Hamburger.js
+++ static/js/components/Hamburger.js
@@ -1,1 +1,35 @@
+/**
+ * ハンバーガーメニュー
+ *
+ * @param {Function} onToggle
+ * @returns {HTMLButtonElement}
+ */
+export function Hamburger(onToggle) {
 
+    const button = document.createElement("button");
+
+    button.className = "hamburger";
+
+    button.innerHTML = `
+        <span></span>
+        <span></span>
+        <span></span>
+    `;
+
+    let isOpen = false;
+
+    button.addEventListener("click", () => {
+
+        isOpen = !isOpen;
+
+        button.classList.toggle("active", isOpen);
+
+        if (typeof onToggle === "function") {
+            onToggle(isOpen);
+        }
+
+    });
+
+    return button;
+
+}
