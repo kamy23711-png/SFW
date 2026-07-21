5b8c110a879c56e2f66cad481df0d23bf82ec72c
kamy23711-png <kamy23711@gmail.com>
2026-07-21 21:54:00 +0900

    Update Card.js

--- static/js/components/Card.js
+++ static/js/components/Card.js
@@ -1,1 +1,21 @@
+export function Card(title,...children){
 
+    const card=document.createElement("section");
+
+    card.className="card";
+
+    const h=document.createElement("h2");
+
+    h.textContent=title;
+
+    card.appendChild(h);
+
+    children.forEach(child=>{
+
+        card.appendChild(child);
+
+    });
+
+    return card;
+
+}
