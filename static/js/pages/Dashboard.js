352d213af9e26bfc01a0bbf0866d06d26a4593dc
kamy23711-png <kamy23711@gmail.com>
2026-07-21 21:52:57 +0900

    Add Dashboard component with Header and Card

--- static/js/pages/Dashboard.js
+++ static/js/pages/Dashboard.js
@@ -1,1 +1,27 @@
+import { Header } from "../components/Header.js";
+import { Card } from "../components/Card.js";
+import { MachineButton } from "../components/MachineButton.js";
 
+export function Dashboard(){
+
+    const page = document.createElement("main");
+
+    page.className = "dashboard";
+
+    page.append(
+
+        Header(),
+
+        Card(
+
+            "NC旋盤",
+
+            MachineButton("M01"),
+
+        )
+
+    );
+
+    return page;
+
+}
